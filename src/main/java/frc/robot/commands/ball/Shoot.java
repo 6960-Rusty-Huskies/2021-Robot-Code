09ae7502d87b6355f22a921dfbe820785414ef29
Steve Forsyth <spforsyth@gmail.com>
2021-03-21 18:25:52 -0700

    Nailed down the shooter P and FF values. Waits till near perfection, then shoots, takes too long, need I & D?

--- src/main/java/frc/robot/commands/ball/Shoot.java
+++ src/main/java/frc/robot/commands/ball/Shoot.java
@@ -7,7 +7,6 @@ import frc.robot.subsystems.ball.*;
 import static frc.robot.Constants.PID.SHOOTER_FF;
 import static frc.robot.Constants.PID.SHOOTER_P;
 import static frc.robot.Constants.Physical.SHOOTER_VELOCITY_15;
-import static frc.robot.subsystems.ball.ShooterSystem.MIN_READY_VELOCITY;
 
 
 public class Shoot extends CommandBase {
@@ -16,9 +15,7 @@ public class Shoot extends CommandBase {
     protected final IndexerSystem upperIndexerSystem;
     protected final IndexerSystem lowerIndexerSystem;
 
-    private boolean initialSetpointHit;
-    private int setpointHitCount = 0;
-    private int initialSetpoint = 0;
+    private int readyStateCount = 0;
     protected int ballCount = 0;
 
     protected boolean lastBeamBreakStatus;
@@ -37,9 +34,8 @@ public class Shoot extends CommandBase {
         // set the intake arm going down, will auto switch off with limit switch
         shooterSystem.setShooterFF(SmartDashboard.getNumber("Shooter FF Value", SHOOTER_FF));
         shooterSystem.setShooterP(SmartDashboard.getNumber("Shooter P Value", SHOOTER_P));
-        setpointHitCount = 0;
-        initialSetpoint = (int) SmartDashboard.getNumber("Shooter RPM Set Value", SHOOTER_VELOCITY_15);
-        shooterSystem.setVelocity(initialSetpoint);
+        readyStateCount = 0;
+        shooterSystem.setVelocity((int) SmartDashboard.getNumber("Shooter RPM Set Value", SHOOTER_VELOCITY_15));
         ballCount = (int) SmartDashboard.getNumber("Power Cell Count", 0);
         lastBeamBreakStatus = upperIndexerSystem.isBeamBreakTriggered();
     }
@@ -47,36 +43,26 @@ public class Shoot extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        if (setpointHitCount < 10) {
+        if (readyStateCount < 20) {
             if (shooterSystem.ready()) {
-                setpointHitCount++;
+                readyStateCount++;
             } else {
-                double ffValue = SmartDashboard.getNumber("Shooter FF Value", SHOOTER_FF);
-                if (shooterSystem.getVelocity() < (initialSetpoint - MIN_READY_VELOCITY)) {
-                    ffValue += .00000005;
-                } else {
-                    ffValue -= .00000005;
-                }
-                shooterSystem.setShooterFF(ffValue);
-                SmartDashboard.putNumber("Shooter FF Value", ffValue);
+                readyStateCount = 0;
             }
         } else {
             boolean upperBeamBreakTriggered = upperIndexerSystem.isBeamBreakTriggered();
             if (lastBeamBreakStatus && !upperBeamBreakTriggered) {
                 ballCount--;
                 SmartDashboard.putNumber("Power Cell Count", ballCount);
-            }
-            lastBeamBreakStatus = upperBeamBreakTriggered;
-
-            if (shooterSystem.ready()) {
-                upperIndexerSystem.drive(.5);
-                lowerIndexerSystem.drive(.5);
-            } else {
                 upperIndexerSystem.drive(0);
                 lowerIndexerSystem.drive(0);
+                readyStateCount = 0;
+            } else {
+                upperIndexerSystem.drive(.5);
+                lowerIndexerSystem.drive(.5);
             }
+            lastBeamBreakStatus = upperBeamBreakTriggered;
         }
-
         shooterSystem.calculate();
     }
 
